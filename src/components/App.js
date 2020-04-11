03d2c12125ae67e28764ffd3e01bf346e0a6e263
Lavanya Singh <singhlavanya94@gmail.com>
2020-04-11 18:14:17 +0530

    add favourites

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,68 @@
 import React from 'react';
+import Navbar from './Navbar';
+import {data} from '../data';
+import MovieCard from "./MovieCard";
+import {addMovies, setShowFavourites} from "../actions";
 
-function App() {
-  return (
-    <div className="App">
-      Project Setup
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount(){
+    const {store}= this.props;
+    store.subscribe(() =>{
+      console.log("Updated");
+      this.forceUpdate();
+    })
+    //make an api call
+    //dispatch action
+    store.dispatch(addMovies(data));
+
+    console.log('State',this.props.store.getState());//{list:[],favourites:[]}
+  }
+  isMovieFavourite  = (movie) =>{
+    const {favourites} = this.props.store.getState();
+
+    const index = favourites.indexOf(movie);
+
+    if(index!==-1){
+      //we found the movie
+      return true;
+    }
+    return false;
+  }
+  onChangeTab = (val) =>{
+    this.props.store.dispatch(setShowFavourites(val));
+  }
+
+  render(){
+  const {list, favourites, showFavourites} = this.props.store.getState();
+  console.log("Rendered");
+  const displayMovies  = showFavourites?favourites:list
+    return (
+      <div className="App">
+        <Navbar />
+        <div className = "main">
+          <div className = "tabs">
+            <div className = {`tab ${showFavourites ? '': 'active-tabs'}`} onClick = {() => this.onChangeTab(false)}>
+              Movies
+            </div>
+            <div className = {`tab ${showFavourites ? 'active-tabs': ''}`} onClick = {() => this.onChangeTab(true)}>
+              Favourites
+            </div>
+            </div>
+            <div className = "list">
+            {displayMovies.map((movie,index) =>
+                (<MovieCard 
+                  movie = {movie}
+                  key = {`movies - ${index}`}
+                  dispatch = {this.props.store.dispatch}
+                  isFavourite  = {this.isMovieFavourite(movie)}
+                  />)
+              )}
+            </div>
+            {displayMovies.length ===0? <div className = "no-movies"> No movies to display! </div>: null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
